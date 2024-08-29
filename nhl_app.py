f72876e90f1b0c1116f5b04f11707cfcc49e22c9
John Rohloff <jmr049@usask.ca>
2024-08-29 11:47:50 -0600

    Updated docstring + documentation in nhl_app.py while also removing formatting warnings

--- nhl_app.py
+++ nhl_app.py
@@ -1,45 +1,55 @@
 import dash
-from dash import dcc
-from dash import html
-
-import model
 from model import NHLModel, FantasyModel
 from view import NHLView
 from controller import NHLController
 
 
 class NHLApp:
+    """
+    This is the main application class which initializes the Dash app, models and view
+
+    Attributes:
+        app (dash.Dash): Dash application instance
+        nhl_model (NHLModel): Model responsible for real nhl statistics
+        fantasy_model (FantasyModel): Model responsible for fantasy statistics
+        view (NHLView): View responsible for the Dash layout and UI
+        controller (NHLController): Responsible for managing interactions between model + view
+    """
     def __init__(self, files):
         """
-        Main application for the NHL player/fantasy stats Dash app
-        It initializes the model, view and controller and sets up the layout and
-        callbacks for the app.
+        Initializes the NHLApp class with provided data files
 
-        :param files: List of filenames (String) without .csv to be loaded by the model
+        Parameters:
+            files (list): List of file names, each containing nhl data of a specific year
         """
+
         # Initialize the Dash app
         self.app = dash.Dash(__name__)
+
+        #Initialize models
         self.nhl_model = NHLModel(files)
         self.fantasy_model = FantasyModel(files)
+
+        #Initialize view with models
         self.view = NHLView(self.nhl_model, self.fantasy_model)
+
+        #Initialize controller with app, models, view
         self.controller = NHLController(self.app, self.nhl_model, self.fantasy_model, self.view)
 
         # Set up the layout
         self.app.layout = self.view.create_layout()
 
-
     def run(self):
         """
         Runs the dash server
-        :param debug:
-        :return:
+
+        Returns: None
         """
         self.app.run_server(debug=True)
 
+
+#Starts the application and provides the required data files for the server.
 if __name__ == '__main__':
-    """
-    Starts the application and provides the required data files for the server.
-    """
     files = ['skaters_23', 'skaters_22']
     app = NHLApp(files)
     app.run()
